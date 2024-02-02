420b920141c0fc9ddce39840a9d83733848d8b33
Vishv Patel <vishv@Vishvs-MacBook-Air.local>
2024-02-02 18:51:02 +0530

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,125 @@
+const logotext = "VISHV";
+const meta = {
+    title: "Vishv Patel",
+    description: "I’m Vishv Patel Full stack devloper",
+};
+
+const introdata = {
+    title: "I’m Vishv Patel",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop desktop software",
+    },
+    description: "I am a well-rounded professional driven by a fervor for digital creativity and innovation. Possessing a robust expertise in Software Development, Mobile App Development, and web development, I am equipped to assist you in realizing your online objectives. Whether you seek to elevate your digital footprint, refine your website, or create a state-of-the-art application, I am well-prepared to meet your needs. 💡",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "A Brief Introduction of Myself",
+    aboutme: "I am a committed expert with a demonstrated history of success in 🌐 digital marketing, 💻 web development, and 🎨 design. My enthusiasm lies in formulating impactful online strategies and crafting captivating digital experiences. My objective is to facilitate the success of businesses and individuals in the digital realm. Emphasizing 🌟 quality, 🗨️ clear communication, and surpassing expectations, I am poised to transform your digital aspirations into reality. Let's collaborate and attain online success! 💡",
+};
+const worktimeline = [{
+        jobtitle: "Designer of week",
+        where: "YAdfi",
+        date: "2020",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "Jamalya",
+        date: "2019",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "ALquds",
+        date: "2019",
+    },
+];
+
+const skills = [
+    {
+        name: "Website Development",
+        value: 100,
+    },
+    {
+        name: "React",
+        value: 95,
+    },
+    {
+        name: "Javascript",
+        value: 95,
+    },
+    {
+        name: "Jquery",
+        value: 85,
+    },
+    {
+        name: "UI & UX Design",
+        value: 80,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "I'm skilled in creating logos, vector art, print designs, flyers, ad banners, posters, and more, providing impactful visual branding for your needs.",
+    },
+    {
+        title: "Web Design",
+        description: "I specialize in developing websites that are easy to use and visually pleasing, with a strong emphasis on UI/UX design to guarantee an outstanding user experience.",
+    },
+    {
+        title: "Desktop Software Development",
+        description: "I stand out in crafting personalized desktop software solutions designed to meet your exact requirements, streamlining and optimizing your operations.",
+    },
+    {
+        title: "Web Development",
+        description: "I possess expertise in website development using platforms like WordPress, Wix, Webflow, and Shopify. Proficient in both frontend and backend development, I offer comprehensive solutions to meet your web development needs.",
+    },
+    {
+        title: "Wordpress Design",
+        description: "I bring expertise in crafting visually appealing and functional websites. From intuitive user interfaces to seamless navigation, I ensure your WordPress site stands out, offering both aesthetic appeal and optimal user experience. ",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://firebasestorage.googleapis.com/v0/b/vishvpatel-5fca7.appspot.com/o/RealEstate.png?alt=media&token=b93cdf05-4fb9-435d-9936-b8fe76b2d8c9",
+        description: "RealEstate is the best place to find your next perfect place to live.",
+        link: "https://realestate-63lh.onrender.com/",
+    },
+    {
+        img: "https://firebasestorage.googleapis.com/v0/b/vishvpatel-5fca7.appspot.com/o/Weather.png?alt=media&token=5b300408-00d1-437c-aa27-7e07bf9fc825",
+        description: "Get The Latest Weather Info Of Your City",
+        link: "https://weather-cau6.onrender.com/",
+    },
+    
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "vishvpatel010@gmail.com",
+    YOUR_FONE: "+91 8866001904",
+    description: "Ready to elevate your digital footprint? Let's work together to turn your online aspirations into tangible success. Reach out today to start a conversation about how I can support you on your journey.",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_4vr3ver",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/vishvpatel010",
+    facebook: "https://www.facebook.com/vishvpatel010",
+    linkedin: "https://www.linkedin.com/in/vishvpatel010",
+    twitter: "https://twitter.com/vishvpatel010",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
